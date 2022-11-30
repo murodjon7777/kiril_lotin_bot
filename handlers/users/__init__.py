3f533f4541fb810c52711e25c23a5ce9af03d151
murodjon7777 <88221222+murodjon7777@users.noreply.github.com>
2022-11-30 21:29:49 +0500

    initial commit

---
+++ handlers/users/__init__.py
@@ -0,0 +1,7 @@
+from . import help
+from . import start
+from . import update_db
+from . import admin
+from . import kl
+# from . import transliterate
+from . import echo
---
+++ handlers/users/admin.py
@@ -0,0 +1,37 @@
+import asyncio
+
+from aiogram import types
+
+from data.config import ADMINS
+from loader import dp, db, bot
+
+@dp.message_handler(text="/allusers", user_id=ADMINS)
+async def get_all_users(message: types.Message):
+    users = db.select_all_users()
+    print(users[0][0])
+    await message.answer(users)
+
+@dp.message_handler(text="/reklama", user_id=ADMINS)
+async def send_ad_to_all(message: types.Message):
+    users = db.select_all_users()
+    active = 0
+    unactive = 0
+    for user in users:
+        try:
+            user_id = user[0]
+            await bot.send_message(chat_id=user_id, text="@SariqDev kanaliga obuna bo'ling!")
+            active += 1
+            await asyncio.sleep(0.05)
+        except:
+            unactive += 1
+    await bot.send_message(
+        chat_id=ADMINS[0],
+        text = f"Active {active} Unactive: {unactive}"
+    )
+        
+        
+
+@dp.message_handler(text="/cleandb", user_id=ADMINS)
+async def get_all_users(message: types.Message):
+    db.delete_users()
+    await message.answer("Baza tozalandi!")
