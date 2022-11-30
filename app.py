5170b69c9cc75ea36885ad369102cf063c0b5f4d
murodjon7777 <88221222+murodjon7777@users.noreply.github.com>
2022-11-30 21:40:06 +0500

    update

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import middlewares, filters, handlers
 from utils.notify_admins import on_startup_notify
 from utils.set_bot_commands import set_default_commands
 
-
+#fgnghcmh
 async def on_startup(dispatcher):
     # Birlamchi komandalar (/star va /help)
     await set_default_commands(dispatcher)
