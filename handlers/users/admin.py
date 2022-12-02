35ab2fb75b98e411d942115b12673231103a186b
murodjon7777 <88221222+murodjon7777@users.noreply.github.com>
2022-12-02 23:26:53 +0500

    state

--- handlers/users/admin.py
+++ handlers/users/admin.py
@@ -18,12 +18,14 @@ async def rek(message:types.Message):
 @dp.message_handler(state=PersonalData.reklama, user_id=ADMINS)
 async def send_ad_to_all(message: types.Message,state=FSMContext):
     users = db.select_all_users()
+    text1=message.text
     active = 0
     unactive = 0 
+    print(text1)
     for user in users:
         try:
             user_id = user[0]
-            await bot.send_message(chat_id=user_id, text=types.message)
+            await bot.send_message(chat_id=user_id, text=text1)
             active += 1
             await asyncio.sleep(0.05)
         except:
@@ -32,6 +34,7 @@ async def send_ad_to_all(message: types.Message,state=FSMContext):
         chat_id=ADMINS[0],
         text = f"Active {active} Unactive: {unactive}"
     )
+    await state.finish()
         
         
 
