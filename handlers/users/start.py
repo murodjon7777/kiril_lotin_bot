01a083cc3aa5f61464cae178483c28aad24510b0
murodjon7777 <88221222+murodjon7777@users.noreply.github.com>
2022-12-02 20:38:51 +0500

    update

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -17,7 +17,7 @@ async def bot_start(message: types.Message):
     except sqlite3.IntegrityError as err:
         await bot.send_message(chat_id=ADMINS[0], text=err)
 
-    await message.answer("Xush kelibsiz!")
+    await message.answer("Xush kelibsiz!!!")
     # Adminga xabar beramiz
     count = db.count_users()[0]
     msg = f"{message.from_user.full_name} bazaga qo'shildi.\nBazada {count} ta foydalanuvchi bor."
